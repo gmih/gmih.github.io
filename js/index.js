80ba346e6f7fdbde71aaca46d91accff3b6a17b8
Heather Tovey <heather@htovey.com>
2016-09-01 15:53:17 -0600

    Convert to jekyll

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-/**
- * Main JS file for Casper behaviours
- */
-
 /* globals jQuery, document */
 (function ($, undefined) {
     "use strict";
@@ -46,3 +42,35 @@
 
     };
 })(jQuery);
+
+
+var anchorForId = function (id) {
+  var anchor = document.createElement("a");
+  anchor.className = "header-link";
+  anchor.href      = "#" + id;
+  anchor.innerHTML = "<i class=\"fa fa-link\"></i>";
+  return anchor;
+};
+
+var linkifyAnchors = function (level, containingElement) {
+  var headers = containingElement.getElementsByTagName("h" + level);
+  for (var h = 0; h < headers.length; h++) {
+    var header = headers[h];
+
+    if (typeof header.id !== "undefined" && header.id !== "") {
+      header.insertBefore(anchorForId(header.id), header.childNodes[0]);
+    }
+  }
+};
+
+document.onreadystatechange = function () {
+  if (this.readyState === "complete") {
+    var contentBlock = document.getElementsByClassName("docs")[0];
+    if (!contentBlock) {
+      return;
+    }
+    for (var level = 2; level <= 2; level++) {
+      linkifyAnchors(level, contentBlock);
+    }
+  }
+};
